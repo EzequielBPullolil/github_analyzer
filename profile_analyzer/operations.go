80469159795b5f9c9126c423f8188953ce98b7dc
EzequielBPullolil <programacionezequielk@gmail.com>
2024-04-06 15:20:51 -0300

    Added AnalyzeProfile operation

--- profile_analyzer/operations.go
+++ profile_analyzer/operations.go
@@ -1,15 +1,69 @@
 package profileanalyzer
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"strings"
+
+	"github.com/EzequielBPullolil/github_analyzer/colors"
+	"github.com/gocolly/colly"
+)
 
 func searchAllRepositories() []Repository {
 	var repos []Repository
+	state := colors.Green("found")
+	c := colly.NewCollector()
+	c.OnHTML("a[itemprop]", func(e *colly.HTMLElement) {
+		var r Repository
 
+		r.Name = strings.TrimSpace(e.Text)
+		r.Url = fmt.Sprintf("https://github.com/%s", e.Attr("href"))
+		repos = append(repos, r)
+	})
+	c.Visit("https://github.com/EzequielBPullolil?tab=repositories")
+	if len(repos) == 0 {
+		state = colors.Fail("not found")
+	}
+	fmt.Printf("Public repositories %s, quantity: %d \n", state, len(repos))
 	return repos
 }
 
+func AnalyzePublicRepos(rs []Repository) (int, int, int) {
+	fmt.Println("Calculate public repos score....")
+	total_commits := 0
+	total_no_readme_repos := 0
+	for _, repo := range rs {
+		if !repo.HaveReadme() {
+			total_no_readme_repos += 1
+		}
+
+		total_commits += repo.CalculateCommits()
+
+	}
+
+	fmt.Println("Public repos score calculated")
+	fmt.Println("-----------------------------")
+	return len(rs), total_commits, total_no_readme_repos
+}
+
+func ProfileHaveReadme(username string) string {
+	r, _ := http.Get(fmt.Sprintf("https://github.com/%s/%s", username, username))
+	if r.StatusCode == 404 {
+		return colors.Fail("False")
+	}
+
+	return colors.Green("True")
+}
+
 func AnalyzeProfile(username string) {
+	fmt.Println(colors.Info("Finding public repos....."))
 	repos := searchAllRepositories()
 
-	fmt.Println(repos)
+	cant_repos, total_commits, total_no_readme_repos := AnalyzePublicRepos(repos)
+	c_average := total_commits / cant_repos
+	fmt.Printf("Repositories without readme: %d/%d \n", total_no_readme_repos, cant_repos)
+	fmt.Printf("Commit average: %d \n", c_average)
+	fmt.Println("Empty repositories: 10")
+	fmt.Printf("Profile Readme: %s \n", ProfileHaveReadme(username))
+	fmt.Println(colors.Info("------------End------------"))
 }
--- profile_analyzer/repository.go
+++ profile_analyzer/repository.go
@@ -1,4 +1,42 @@
 package profileanalyzer
 
+import (
+	"fmt"
+	"strconv"
+	"strings"
+
+	"github.com/gocolly/colly"
+)
+
 type Repository struct {
+	Url, Name string
+}
+
+func (e *Repository) CalculateCommits() int {
+	cant_commits := 0
+	c := colly.NewCollector()
+	c.OnHTML("span.Text-sc-17v1xeu-0", func(h *colly.HTMLElement) {
+		sCommits, f := strings.CutSuffix(h.Text, "Commits")
+		if f {
+			t, _ := strings.CutSuffix(sCommits, "Commits")
+			final := strings.TrimSpace(t)
+			n, _ := strconv.Atoi(final)
+			cant_commits = n
+		}
+	})
+	c.Visit(e.Url)
+	return cant_commits
+}
+
+func (e Repository) HaveReadme() bool {
+	var haveReadme = true
+	c := colly.NewCollector()
+	c.OnHTML("span.Text-sc-17v1xeu-0", func(h *colly.HTMLElement) {
+		if h.Attr("class") == "Text-sc-17v1xeu-0 eJOnBv" {
+
+			fmt.Println(h.Text)
+		}
+	})
+	c.Visit(e.Url)
+	return haveReadme
 }
